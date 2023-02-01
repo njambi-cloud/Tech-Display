b3d7d6d9cfa709eb9e0919ccf906ac785531b35d
njambi-cloud <christinemacharia64@gmail.com>
2023-02-01 09:25:45 +0300

    Adds the about component and gives it a route in App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Home from "./Home";
+import About from "./About";
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <Routes >
+         <Route exact path="/about" element={<About/>} />
          <Route exact path="/" element={<Home/>} />
       </Routes>
     </div>
