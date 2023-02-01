6787c3c994a0cbe9637d024e0f4afd3c315abff1
njambi-cloud <christinemacharia64@gmail.com>
2023-02-01 09:32:08 +0300

    Creates a tech list to display the list of tech

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Home from "./Home";
 import About from "./About";
+import TechList from "./TechList";
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Routes >
          <Route exact path="/about" element={<About/>} />
+         <Route exact path="/trendlist" element={<TechList/>} />
          <Route exact path="/" element={<Home/>} />
       </Routes>
     </div>
