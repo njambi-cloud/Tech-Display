071bfc1abc329fc19e0fe523b4636bda59461548
njambi-cloud <christinemacharia64@gmail.com>
2023-02-01 09:49:43 +0300

    Adds the Home page

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function Home () {
+
+    return (
+        <div className="home-header">
+            <div className="home-nav">
+                <h1>Tech Display</h1>
+            </div>
+        
+            <div className="home">
+                <h2>Be upto date with the current trends in technology</h2>
+                <h4>The field of technology is one which keeps changing every minute. There are very many trends in technology today which everyone needs to know</h4>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Home
